e838877b4ea8cd1a040c9c9c35a80de11b59a8ba
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-02-11 09:29:14 -0800

    project router calls working through delete

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,40 @@
 // add middlewares here related to projects
+const Projects = require('./projects-model');
+
+function logger(req, res, next) {
+  console.log(`${new Date().toISOString()} : [${req.method}] at ${req.url}`);
+  next();
+};
+
+async function validateProjId(req, res, next) {
+  const { id } = req.params;
+  try {
+    const project = await Projects.get(id);
+    if (!project) {
+      res.status(404).json({ message: "Project not found..."});
+    } else {
+      req.project_id = project;
+      next();
+    }
+  } catch(err) {
+    res.status(500).json({ message: "Error finding the project..."});
+  }
+};
+
+async function validateProject(req, res, next) {
+  const { name, description } = req.body;
+  if (!name || !description) {
+    res.status(400).json({ message: "Please enter both a project name and description..." });
+  } else {
+    req.name = name;
+    req.description = description;
+    next();
+  }
+};
+
+
+module.exports = {
+  logger,
+  validateProjId,
+  validateProject,
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,54 @@
-// Write your "projects" router here!
+const express = require('express');
+const Projects = require('./projects-model');
+
+const { logger, validateProjId, validateProject } = require('./projects-middleware');
+const router = express.Router();
+
+// Pull in middleware
+
+// Endpoints
+router.get('/', logger, (req, res, next) => {
+  Projects.get()
+    .then(project => {
+      res.json(project);
+    })
+    .catch(next);
+});
+
+router.get('/:id', logger, validateProjId, (req, res, next) => {
+  Projects.get(req.params.id)
+    .then(project => {
+      res.json(project);
+    })
+    .catch(next);
+});
+
+router.post('/', logger, validateProject, (req, res, next) => {
+  Projects.insert(req.body)
+    .then(newProj => {
+      res.status(201).json(newProj);
+    })
+    .catch(next);
+});
+
+router.put('/:id', logger, validateProjId, validateProject, (req, res, next) => {
+  Projects.update(req.params.id, req.body)
+    .then(updatedProj => {
+      if (req.body.completed === null || req.body.completed === undefined) {
+        res.status(400).json({ message: "please enter a completed status..." });
+      } else {
+        res.status(200).json(updatedProj);
+      }
+    })
+    .catch(next);
+});
+
+router.delete('/:id', logger, validateProjId, (req, res, next) => {
+  Projects.remove(req.params.id)
+    .then(delProject => {
+      res.json(delProject);
+    })
+    .catch(next);
+});
+
+module.exports = router;
