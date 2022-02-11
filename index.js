3621708597efa4996e6c58066ec6116ba9b8a008
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-02-11 08:15:26 -0800

    set up server, .env, installed dependencies

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+require('dotenv').config();
+const express = require('express');
+
+const server = express();
+
+const PORT = process.env.PORT || 9000;
+
+server.listen(PORT,  () => {
+  console.log(`server listening on port ${PORT}...`);
+});
